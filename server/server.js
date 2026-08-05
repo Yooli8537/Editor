d8fbdba05d32463ee0e1854c1359de337af510a2
Yooli8537 <Yooli8537@gmx.ch>
2026-08-05 16:03:09 +0200

    Created images & attachments folder for future features

--- server/server.js
+++ server/server.js
@@ -5,26 +5,39 @@ const path = require("path");
 const app = express();
 const port = 8510;
 
-const dataFolder = path.join(__dirname, "../data");
+const rootPath = path.join(__dirname, "../");
+const dataFolder = path.join(rootPath, "data");
+const imageFolder = path.join(rootPath, "images");
+const attachmentsFolder = path.join(rootPath, "attachments");
 
-if (!fs.existsSync(dataFolder)) {
-  fs.mkdirSync(dataFolder);
-  console.warn("Created missing Data Folder.");
-  console.log(
-    "This is standard if you've freshly cloned the Repository, as the data folder is ignored by git.",
-  );
+const userDataFolders = [
+  { name: "Data", path: dataFolder },
+  { name: "Image", path: imageFolder },
+  { name: "Attachments", path: attachmentsFolder },
+];
+
+for (let i = 0; i < userDataFolders.length; i++) {
+  if (!fs.existsSync(userDataFolders[i].path)) {
+    fs.mkdirSync(userDataFolders[i].path);
+    console.warn(
+      `Created missing ${userDataFolders[i].name} Folder.`,
+    );
+    console.log(
+      "This is standard if you've freshly cloned the Repository, as the data folder is ignored by git.",
+    );
+  }
 }
 
 const exportRoute = require("./routes/export");
 const documentsRoute = require("./routes/documents");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(rootPath));
 app.use(exportRoute);
 app.use(documentsRoute);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "index.html"));
+  res.sendFile(path.join(rootPath, "index.html"));
 });
 
 app.listen(port, () => {});
