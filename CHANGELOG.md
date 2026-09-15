b2c1af9a26b9eec5cd3765797f8cbdb512dd976a
Yooli8537 <Yooli8537@gmx.ch>
2026-09-15 09:54:42 +0200

    v1.6.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v1.6.1 - Security patches & minor Improvements
 
 ### Added
 
--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ const allProperties = {
   successLogs: true,
   saveLogs: false,
   confirmExport: false,
-  version: "v1.6.0",
+  version: "v1.6.1",
   deniedVersion: null,
   clientActionLogging: false,
   rateLimitResetTime: 5,
